150af36fa8f12f01446c4d4960eb61780f686c5d
Shamik <shamikghosh678@gmail.com>
2025-01-02 19:46:26 +0530

    Converts base(10) numbers to base(x)

---
+++ anyBase.c
@@ -0,0 +1,25 @@
+/*10.Write a function to convert a decimal number to any other
+base given by the user..*/
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int n,cpy,base,r,i=0;
+    char res[100];
+    char rep[]="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    printf("\nEnter a decimal number: ");
+    scanf("%d",&n);
+    printf("\nEnter any base (<62): ");
+    scanf("%d",&base);
+    cpy=n;
+    while(n!=0)
+    {
+        r=n%base;
+        res[i++]=rep[r];
+        n/=base;
+    }
+    res[i]='\0';
+    printf("\nDecimal number %d converted to base %d is: %s",cpy,base,strrev(res));
+    return 0;
+}
